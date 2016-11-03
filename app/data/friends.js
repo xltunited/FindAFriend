9e6707d076f8b797001e5d749a391ad38f6264a5
Eddy Leon <xltunited@gmail.com>
2016-11-02 22:46:57 -0400

    finished

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,3 @@
-
 var friendArray = [
 
   {
@@ -17,7 +16,163 @@ var friendArray = [
      4,
      1
    ]
-  }
+  },
+
+  {
+
+  "name":"Tomasa",
+  "photo":"http://cdn2.stylecraze.com/wp-content/uploads/2013/05/tomato-hair-benefits1.jpg",
+  "scores":[
+     1,
+     2,
+     5,
+     4,
+     1,
+     5,
+     1,
+     4,
+     4,
+     5
+   ]
+  },
+
+    {
+
+  "name":"Margaret",
+  "photo":"http://thecookful.com/wp-content/uploads/2015/09/Better-Margarita-portrait-680.jpg",
+  "scores":[
+     2,
+     5,
+     4,
+     3,
+     5,
+     3,
+     2,
+     4,
+     4,
+     2
+   ]
+  },
+    {
+
+  "name":"Alfredo",
+  "photo":"http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2011/2/4/1/RX-FNM_030111-Lighten-Up-012_s4x3.jpg.rend.sniipadlarge.jpeg",
+  "scores":[
+     1,
+     2,
+     3,
+     4,
+     5,
+     1,
+     2,
+     3,
+     4,
+     5
+   ]
+  },
+    {
+
+  "name":"Caesar",
+  "photo":"http://stonefiregrill.com/wp-content/uploads/2014/08/SFG-Menu-Caesar-Salad.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+  },
+    {
+
+  "name":"Candy",
+  "photo":"http://d3vuec9wjh8whu.cloudfront.net/media/catalog/category/c_16b1ae86769eca54fe231c65c01ceb33_By-Type.jpg",
+  "scores":[
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5
+   ]
+  },
+    {
+
+  "name":"Tiffany",
+  "photo":"https://www.worldsmostfamoustaffy.com/wp-content/uploads/2014/07/pickyourownmix.png",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+  },
+    {
+
+  "name":"Chip",
+  "photo":"http://cdn-mf1.heartyhosting.com/sites/mensfitness.com/files/d6/2010-images-junk-food-0710-junk-food-chips.jpg",
+  "scores":[
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1
+   ]
+  },
+    {
+
+  "name":"Olive",
+  "photo":"http://static.communitytable.parade.com/wp-content/uploads/2014/06/june-first-bite-olives-ftr.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+  },
+    {
+
+  "name":"Stewie",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     2,
+     5,
+     5,
+     3,
+     5,
+     1,
+     2,
+     3,
+     3,
+     2
+   ]
+  },
+  
  
 ];
 
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -9,8 +9,38 @@ module.exports = function (app) {
 	});
 
 	app.post('/api/friends', function (req, res) {
-	
-		console.log(req.body);
+
+		var result = [req.body.questionOne, req.body.questionTwo, req.body.questionThree, req.body.questionFour, req.body.questionFive, req.body.questionSix, req.body.questionSeven, req.body.questionEight, req.body.questionNine, req.body.questionTen];
+		
+		var mostCompatable = 50;
+
+		var index = -1;
+
+		for(var i = 0; i < friendListData.length; i++){
+
+			var temp = 0;
+
+			for(var j = 0; j < friendListData[i].scores.length; j++){
+
+				temp = temp + Math.abs(friendListData[i].scores[j] - result[j]);
+
+				console.log(temp);
+
+			}
+
+			if(temp < mostCompatable){
+
+				mostCompatable = temp;
+
+				index = i
+
+			}
+
+		}
+
+		friendListData.push({name:req.body.FLname, photo: req.body.Picture, score:result});
+
+		res.send('<p>Your Best Friend is ' + friendListData[index].name + '</p><img src="' + friendListData[index].photo + '">');
 
 	});
 
